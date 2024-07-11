7766bc235d553dbfdfcf0e00a9a1c993ad5f3dd9
Vlashex <rmaynavine@gmail.com>
2024-07-11 15:25:13 +0300

    скрыл ключи

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCponMBtBr3nttsJ75qZHhiJFhXBD139P0",
-  authDomain: "shopco-68498.firebaseapp.com",
-  projectId: "shopco-68498",
-  storageBucket: "shopco-68498.appspot.com",
-  messagingSenderId: "305769836039",
-  appId: "1:305769836039:web:22c5bbac26955ac61884a2",
-  measurementId: "G-EWSVM7FPZ5"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
+
+export {db}
