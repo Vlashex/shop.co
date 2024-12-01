c16860b2091ffa3d3b6d4abab3b2c536997518c7
Vlashex <rmaynavine@gmail.com>
2024-12-01 14:01:32 +0300

    Исправил вывод ошибки при регисттрации

--- src/app/(Auth)/SignUp/actions/SignUpAction.ts
+++ src/app/(Auth)/SignUp/actions/SignUpAction.ts
@@ -1,16 +1,32 @@
 "use server"
 import { hashValue } from '@/lib/functions/hashValue';
-import { IRegister, IUser } from '@/lib/types';
-import axios from 'axios'
+import { IAuth, IRegister } from '@/lib/types';
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
+interface Err {
+  statusCode: number
+  message: string
+}
+
+interface Responce {
+  data: IAuth | null,
+  error: Err | null
+}
 
 export const SignUpAction = async ({email, name, password}: IRegister) => {
-  
-  return await axios.post('http://localhost:4200/api/users/signup', {
+    const responce: Responce = await axios.post('http://localhost:4200/api/users/signup', {
     name:name,
     email:email,
     password: hashValue(password),
   })
-  .then((res) => res.data)
-  .catch((err) => console.log(err))
+  .then((resp: AxiosResponse<IAuth>) => {return{
+      data: resp.data,
+      error: null
+    }})
+  .catch((error:AxiosError<Err>) => {return{
+      data: null,
+      error: error.response?.data || null
+    }})
+
+  return responce
 }
--- src/app/(Auth)/SignUp/ui/SignUpUi.tsx
+++ src/app/(Auth)/SignUp/ui/SignUpUi.tsx
@@ -1,81 +1,114 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { FormField } from '@/features/FormField'
-import { SignUpAction } from '../actions/SignUpAction'
-import { useDispatch } from 'react-redux'
-import { setCredentials } from '@/lib/store/authSlice'
-import { useRouter } from 'next/navigation'
-import { IRegister } from '@/lib/types'
-import { useCookies } from 'react-cookie'
-import { createHash } from 'crypto'
-import { hashValue } from '@/lib/functions/hashValue'
-
-
+"use client";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormField } from "@/features/FormField";
+import { SignUpAction } from "../actions/SignUpAction";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "@/lib/store/authSlice";
+import { useRouter } from "next/navigation";
+import { IRegister } from "@/lib/types";
+import { useCookies } from "react-cookie";
+import { createHash } from "crypto";
+import { hashValue } from "@/lib/functions/hashValue";
 
 export default function SignUpUi() {
-
-  const [isUserExist, setUserExist] = useState<boolean>(false)
-  const dispatch = useDispatch()
-  const { replace } = useRouter()
-  const [ cookies, setCookies ] = useCookies(['access_token', 'refresh_token'])
+  const [registerError, setRegisterError] = useState<string | null>(null);
+  const dispatch = useDispatch();
+  const { replace } = useRouter();
+  const [cookies, setCookies] = useCookies(["access_token", "refresh_token"]);
 
   const UserSchema = z
-  .object({
-    email: z
-      .string()
-      .email(),
-    name: z
-      .string()
-      .min(3),
-    password: z
-      .string()
-      .min(6, { message: "Password is too short" })
-      .max(20, { message: "Password is too long" }),
-    confirmedPassword: z
-      .string()
-  })
-  .refine((data) => data.password === data.confirmedPassword, {
-    message: "Passwords do not match",
-    path: ["confirmedPassword"], // path of error
-  });
+    .object({
+      email: z.string().email(),
+      name: z.string().min(3),
+      password: z
+        .string()
+        .min(6, { message: "Password is too short" })
+        .max(20, { message: "Password is too long" }),
+      confirmedPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmedPassword, {
+      message: "Passwords do not match",
+      path: ["confirmedPassword"], // path of error
+    });
 
   interface SignUp extends IRegister {
-    confirmedPassword: string
+    confirmedPassword: string;
   }
 
-  const { register, handleSubmit, formState: { errors } } = useForm<SignUp>({resolver: zodResolver(UserSchema)})
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUp>({ resolver: zodResolver(UserSchema) });
 
-  const onSubmit = (data:SignUp) => {
-    const {confirmedPassword, ...res} = data
-    res.password = hashValue(res.password)
-  	const signUp = async() => {
-      const result = await SignUpAction(res)
-      if ( result == null) setUserExist(true)
-      else {
-        dispatch(setCredentials(result))
-        setCookies('access_token', result.tokens.access_token)
-        setCookies('refresh_token', result.tokens.refresh_token)
-        replace('/')
-      }
+  const onSubmit = async(formdData: SignUp) => {
+    const { confirmedPassword, ...cred } = formdData;
+    const credentials:IRegister = {
+      ...cred,
+      password: hashValue(formdData.password),
+    };
+
+    const { error, data } = await SignUpAction(credentials);
+
+    if (error !== null) {
+      setRegisterError(error.message);
+      return;
     }
-    signUp()
-  }
+    if (data !== null) {
+      dispatch(setCredentials(data));
+      setCookies("access_token", data.tokens?.access_token);
+      setCookies("refresh_token", data.tokens?.refresh_token);
+      replace("/");
+    }
+    if (data === null && error === null) {
+      setRegisterError("Something wrong")
+    }
+  };
 
   return (
-    <div className='w-[400px] bg-white flex flex-col gap-8 rounded-xl overflow-hidden justify-center items-center px-8 py-16'>
-        <h1 className='text-2xl'>Registration</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 w-full'>
-            <FormField type='name'     name='name'               register={register}   error={errors.name}          placeholder='Enter your name'/>
-            <FormField type='email'    name='email'              register={register}   error={errors.email}             placeholder='Enter your email'/>
-            {isUserExist? <label className='-mt-4 text-red-500'>User is already exist</label>: null}
-            <FormField type='password' name='password'           register={register}   error={errors.password}          placeholder='Enter your password'/>
-            <FormField type='password' name='confirmedPassword'  register={register}   error={errors.confirmedPassword} placeholder='Confirm password'/>
-            <Button className='bg-black text-white rounded-sm'>SignUp</Button>
-        </form>
+    <div className="w-[400px] bg-white flex flex-col gap-8 rounded-xl overflow-hidden justify-center items-center px-8 py-16">
+      <h1 className="text-2xl">Registration</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-5 w-full"
+      >
+        <FormField
+          type="name"
+          name="name"
+          register={register}
+          error={errors.name}
+          placeholder="Enter your name"
+        />
+        <FormField
+          type="email"
+          name="email"
+          register={register}
+          error={errors.email}
+          placeholder="Enter your email"
+        />
+        {registerError != null ? (
+          <label className="-mt-4 text-red-500">{registerError}</label>
+        ) : null}
+        <FormField
+          type="password"
+          name="password"
+          register={register}
+          error={errors.password}
+          placeholder="Enter your password"
+        />
+        <FormField
+          type="password"
+          name="confirmedPassword"
+          register={register}
+          error={errors.confirmedPassword}
+          placeholder="Confirm password"
+        />
+        <Button className="bg-black text-white rounded-sm">SignUp</Button>
+      </form>
     </div>
-  )
+  );
 }
